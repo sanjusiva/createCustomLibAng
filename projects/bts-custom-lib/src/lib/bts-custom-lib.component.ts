f74642c15386b3096085dc203bbe2906214daa0f
sanjitha.siva <sanjitha.siva@aspiresys.com>
2024-02-14 13:00:44 +0530

    made correction

--- projects/bts-custom-lib/src/lib/bts-custom-lib.component.ts
+++ projects/bts-custom-lib/src/lib/bts-custom-lib.component.ts
@@ -3,21 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'lib-bts-custom-lib',
   template: `
-  <div *ngFor="let idol of bts">
-    <li>
+    <div *ngFor="let idol of bts">
       <ul>
-        {{idol}}
+        <li>
+          {{ idol }}
+        </li>
       </ul>
-    </li>
-  </div>
+    </div>
   `,
-  styles: [
-  ]
+  styles: [],
 })
-export class BtsCustomLibComponent implements OnInit{
-  bts=['Kim Namjoon','Kim SoekJin','Min Yoongi','Jeon Hoesok','Park Jimin','Kim Taehyung','Jeon Jungkook']
-  ngOnInit(){
-
-  }
-
+export class BtsCustomLibComponent implements OnInit {
+  bts = [
+    'Kim Namjoon',
+    'Kim SoekJin',
+    'Min Yoongi',
+    'Jeon Hoesok',
+    'Park Jimin',
+    'Kim Taehyung',
+    'Jeon Jungkook',
+  ];
+  ngOnInit() {}
 }
